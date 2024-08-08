e4145385b2c74bb2cd9da057c6531c4c59b1e8fb
Kristain Putra <kputrapura@gmail.com>
2024-08-08 20:30:23 +0700

    product list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-header",
-  standalone: true, // ignore aja dulu, ini fitur angular 18 -> not necessarry to decalred in ng module
-  templateUrl: "header.component.html", // ngasih tahu html mana, kalau mau plain string pakai template
-  styleUrl: "header.component.css", // can be collection of files using stulesUrls
-  styles: [] // array of inline styles
+    selector:'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent{
+
+}
